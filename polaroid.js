6cf7f28070518b08962b2d88335bea6a9bad42c4
NotPerr <vovo7292@gmail.com>
2023-10-15 17:12:00 +0800

    change frame style function complete

--- polaroid.js
+++ polaroid.js
@@ -2,15 +2,28 @@ const fileInput = document.querySelector('#file_input'),
 uploadBtn = document.querySelector('#upload_image'),
 previewImg = document.querySelector('.preview-img img'),
 generateBtn = document.querySelector('#generate_btn'),
-frame_1 = document.querySelector('#frame_1'),
-saveImgBtn = document.querySelector('#save_img')
+saveImgBtn = document.querySelector('#save_img'),
+choose_frame = document.querySelectorAll('.frame-style')
 
 previewImg.width = 220
 previewImg.height = 277
 
+let frame = document.querySelector('#frame_1')
+let hasGenerate = false  // to check if the user has generated an image or not
 
+// click on frame to change frame style
+const chooseStyle = (id) => {
+    if(!hasGenerate) return
+    console.log(id)
+    frame = document.getElementById(id)
+    draw(frame)
+}
 
+for(let i = 0;i < choose_frame.length;i++) {
+    choose_frame[i].addEventListener('click',() => {chooseStyle(choose_frame[i].id)})
+}
 
+// load upload image
 const loadImage = () => {
     console.log('load image')
     let file = fileInput.files[0]
@@ -19,27 +32,28 @@ const loadImage = () => {
 }
 
 // draw preview canvas
-const draw = () => {
+const draw = (frame) => {
     console.log('draw')
+    hasGenerate = true
     let imgRatio = previewImg.naturalHeight / previewImg.naturalWidth
     let canvas = document.getElementById("img_canvas")
 
     if(506*imgRatio < 620) {
         
-        canvas.width = frame_1.naturalWidth
+        canvas.width = frame.naturalWidth
         canvas.height = (506*imgRatio + 182)
         let ctx = canvas.getContext('2d')
         ctx.fillStyle = "#fff"
         ctx.fillRect(0,0,506,(506*imgRatio + 182))
         ctx.drawImage(previewImg, 19, 38, 468, 506*imgRatio)
-        ctx.drawImage(frame_1, 0, 0, frame_1.naturalWidth, (506*imgRatio + 182))
+        ctx.drawImage(frame, 0, 0, frame_1.naturalWidth, (506*imgRatio + 182))
         
     } else {
-        canvas.width = frame_1.naturalWidth
-        canvas.height = frame_1.naturalHeight
+        canvas.width = frame.naturalWidth
+        canvas.height = frame.naturalHeight
         let ctx = canvas.getContext('2d')
         ctx.drawImage(previewImg, 19, 38, 468, 506*imgRatio)
-        ctx.drawImage(frame_1, 0, 0)
+        ctx.drawImage(frame, 0, 0)
     }
     
 }
@@ -52,29 +66,29 @@ const save = (canvas) => {
     link.click()
 }
 
-// draw result image and save
-const drawImg = () => {
+// draw result image and download
+const drawImg = (frame) => {
     console.log('save image')
     let imgRatio = previewImg.naturalHeight / previewImg.naturalWidth
     let canvas = document.createElement("canvas")
     
     if(506*imgRatio < 620) {
         
-        canvas.width = frame_1.naturalWidth
+        canvas.width = frame.naturalWidth
         canvas.height = 506*imgRatio + 182
         let ctx = canvas.getContext('2d')
         ctx.fillStyle = "#fff"
         ctx.fillRect(0,0,506,506*imgRatio + 182)
         ctx.drawImage(previewImg, 19, 38, 468, 506*imgRatio)
-        ctx.drawImage(frame_1, 0, 0, frame_1.naturalWidth, 506*imgRatio + 182)
+        ctx.drawImage(frame, 0, 0, frame.naturalWidth, 506*imgRatio + 182)
         
     } else {
         
-        canvas.width = frame_1.naturalWidth
-        canvas.height = frame_1.naturalHeight
+        canvas.width = frame.naturalWidth
+        canvas.height = frame.naturalHeight
         let ctx = canvas.getContext('2d')
         ctx.drawImage(previewImg, 19, 38, 468, 506*imgRatio)
-        ctx.drawImage(frame_1, 0, 0)
+        ctx.drawImage(frame, 0, 0)
     }
 
     save(canvas)
@@ -82,5 +96,5 @@ const drawImg = () => {
 
 uploadBtn.addEventListener('click',()=>{fileInput.click()})
 fileInput.addEventListener('change',loadImage)
-generateBtn.addEventListener('click',draw)
-saveImgBtn.addEventListener('click', drawImg)
+generateBtn.addEventListener('click',() => {draw(frame)})
+saveImgBtn.addEventListener('click', () => {drawImg(frame)})
